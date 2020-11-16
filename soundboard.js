24df84a6fe7acaffb0cf35ebbbee088f54e95d43
abriellemason <masonab1@msu.edu>
2020-11-15 22:55:51 -0500

    fixed and ready for review

--- soundboard.js
+++ soundboard.js
@@ -1,10 +1,34 @@
-function PlaySound(soundobj) {
-    var thissound=document.getElementById(soundobj);
-    thissound.play();
+var heart_sound = document.getElementById("heart-a");
+
+var timer_sound = document.getElementById("timer-a");
+
+var sewing_sound = document.getElementById("sewing-a");
+
+function playHeart() {
+   heart_sound.play();
 }
 
-function StopSound(soundobj) {
-    var thissound=document.getElementById(soundobj);
-    thissound.pause();
-    thissound.currentTime = 0;
+function stopHeart() {
+    heart_sound.pause();
+    heart_sound.currentTime=0;
 }
+
+function playTimer() {
+  timer_sound.play();
+}
+
+function stopTimer() {
+    timer_sound.pause();
+    timer_sound.currentTime=0;
+}
+
+function playSewing() {
+   sewing_sound.play();
+}
+
+function stopSewing() {
+    sewing_sound.pause();
+    sewing_sound.currentTime=0;
+}
+
+//Credits go to https://stackoverflow.com/questions/14926306/javascript-play-sound-on-hover-stop-and-reset-on-hoveroff//
